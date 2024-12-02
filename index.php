bfd9ed9a50a3600549b9d817af71be430d3de136
Lauliek <laureline.pellegrini@gmail.com>
2024-12-02 14:41:48 +0100

    c

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-spl_autoload_register(function ($class) {
- if(file_exists('./core/' . $class . '.php')):
- include './core/' . $class . '.php';
- elseif (file_exists('./models/' . $class . '.php')):
- include './models/' . $class . '.php';
- elseif (file_exists('./controllers/' . $class . '.php')):
- include './controllers/' . $class . '.php';
- endif;
-});
+include './autoLoader.php';
+
+
+$testduDAO = new DefaultDAO();
+$testduDAO->getAll();
+
+
